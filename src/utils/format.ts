e968bc92b14b571afd7ad1726d0056353c86bbb8
kujira <ironys@foxmail.com>
2023-01-19 20:21:08 +0800

    fix: 修复格式化算法对换行内容无效的bug

--- src/utils/format.ts
+++ src/utils/format.ts
@@ -3,6 +3,7 @@ import { isTr, isUr, isAr, isZh, isEn } from '@/utils/regMatch';
 import JSBeautify from 'js-beautify';
 // 格式化表格数据
 export function format(index?: string): string {
+  // 获取目标工作簿
   let currentActiveSheet;
   if (index) {
     currentActiveSheet = window.luckysheet
@@ -13,11 +14,16 @@ export function format(index?: string): string {
       .getAllSheets()
       .find((v: any) => v.status).data;
   }
+  console.log(currentActiveSheet);
   // 去除表格空行
   for (let i = currentActiveSheet.length - 1; i >= 0; i--) {
     let nullCount = 0;
     for (let k = 0; k < currentActiveSheet[i].length; k++) {
-      if (currentActiveSheet[i][k] === null || !currentActiveSheet[i][k].v)
+      if (
+        currentActiveSheet[i][k] === null ||
+        (!currentActiveSheet[i][k].v &&
+          !currentActiveSheet[i][k]?.ct?.[0]?.s?.v)
+      )
         nullCount++;
     }
     if (nullCount === currentActiveSheet[i].length) {
@@ -51,7 +57,10 @@ export function format(index?: string): string {
   }
   for (let row = 0; row < currentActiveSheet.length; row++) {
     for (let column = 0; column < currentActiveSheet[row].length; column++) {
-      let value = currentActiveSheet[row][column]?.v;
+      let value =
+        currentActiveSheet[row][column]?.v ||
+        currentActiveSheet[row][column]?.ct?.s?.[0]?.v;
+      console.log(currentActiveSheet[row][column]);
       if (value) {
         value = value.trim();
         columns[column].push(value);
@@ -102,44 +111,62 @@ export function format(index?: string): string {
   let enStr = ``;
   if (result['en']) {
     result['en'].forEach((v, i) => {
-      const wordsArr = v.replace(/[^a-zA-Z0-9 ]/g, ' ').split(' ');
+      const wordsArr = v
+        .replace('-', 'To')
+        .replace(/[^a-zA-Z0-9 ]/g, ' ')
+        .trim()
+        .split(' ');
       let currentKey = '';
-      for (let i = 0; i < Math.min(8, wordsArr.length); i++) {
-        if (!wordsArr[i] || wordsArr[i] === ' ') continue;
-        if (i > 0) {
-          // 去除多余介词和无意义词语，限制Key值长度
-          if (
-            ([
-              'the',
-              'if',
-              'to',
-              'of',
-              'on',
-              'for',
-              'has',
-              'have',
-              'can',
-              'could',
-              'in',
-              'xxx',
-              'xx',
-              'x',
-            ].includes(wordsArr[i].toLowerCase()) &&
-              (currentKey.length + (wordsArr?.[i + 1]?.length || 0) > 21 ||
-                i === Math.min(8, wordsArr.length) - 1)) ||
-            currentKey.length > 21
-          ) {
-            break;
-          }
+      for (let i = 0; i < Math.min(6, wordsArr.length); i++) {
+        // 禁止数字开头，禁止无意义空格
+        if (
+          !wordsArr[i] ||
+          wordsArr[i] === ' ' ||
+          (!currentKey.length && /^[0-9]+$/.test(wordsArr[i]))
+        )
+          continue;
 
-          currentKey += (
-            wordsArr[i].slice(0, 1).toUpperCase() + wordsArr[i].slice(1)
-          ).trim();
-        } else {
-          currentKey += wordsArr[i].trim();
+        // if(/^[0-9]+$/.test(wordsArr[i])) {
+        //   console.log(wordsArr[i], currentKey);
+        // }
+        // 禁止时间数字连体
+        if (
+          /^[0-9]+$/.test(wordsArr[i]) &&
+          (wordsArr[i].length > 5 ||
+            /[0-9]/.test(currentKey[currentKey.length - 1]))
+        )
+          continue;
+        // 去除多余介词和无意义尾词+限制Key值长度
+        if (
+          ([
+            'the',
+            'if',
+            'to',
+            'of',
+            'on',
+            'for',
+            'has',
+            'have',
+            'can',
+            'could',
+            'in',
+            'xxx',
+            'xx',
+            'x',
+            'into',
+          ].includes(wordsArr[i].toLowerCase()) &&
+            (currentKey.length + (wordsArr?.[i + 1]?.length || 0) > 21 ||
+              i === Math.min(8, wordsArr.length) - 1)) ||
+          currentKey.length > 21
+        ) {
+          break;
         }
+        // 首字母大写
+        currentKey += (
+          wordsArr[i].slice(0, 1).toUpperCase() + wordsArr[i].slice(1)
+        ).trim();
       }
-      // 判断是否重复
+      // 判断KEY是否重复
       let repeatNum = 0;
       for (let i = 0; i < key.length; i++) {
         if (
@@ -174,6 +201,5 @@ export function format(index?: string): string {
   // 格式化代码
   const options = { indent_size: 2, space_in_empty_paren: true };
   rawStr = JSBeautify(rawStr, options);
-  // console.log(rawStr);
   return rawStr;
 }
